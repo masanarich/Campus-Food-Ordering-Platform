65e657460705131990416455abef783cd5f3c0fe
Gwen <ledilegwen@gmail.com>
2026-04-02 20:32:11 +0200

    saving the work

---
+++ frontend/js/admin.js
@@ -0,0 +1,28 @@
+const API = "http://localhost:3000";
+
+async function loadVendors() {
+  const res = await fetch(`${API}/vendors`);
+  const data = await res.json();
+
+  const list = document.getElementById("vendorList");
+
+  data.forEach(v => {
+    list.innerHTML += `
+      <div>
+        <p>${v.name} (${v.status})</p>
+        <button onclick="approve(${v.id})">Approve</button>
+        <button onclick="suspend(${v.id})">Suspend</button>
+      </div>
+    `;
+  });   
+}
+
+function approve(id) {
+  fetch(`${API}/vendor/${id}/approve`, { method: "PUT" });
+}
+
+function suspend(id) {
+  fetch(`${API}/vendor/${id}/suspend`, { method: "PUT" });
+}
+
+loadVendors();
