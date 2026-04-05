14670271c7d9dcd27230773b44d9efc03cb9b5a7
Gwen <ledilegwen@gmail.com>
2026-04-05 22:09:57 +0200

    added a new router

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -14,4 +14,12 @@ router.put('/:id/soldout', (req, res) => {
   res.json(markSoldOut(req.params.id));
 });
 
+router.put('/:id',(req,res)=>{
+  try{
+    const updated=updateItem(req.params.id,req.body);
+    res.json(updated);
+  }catch(err){
+    res.status(404).send(err.message);
+  }
+});
 module.exports = router;
