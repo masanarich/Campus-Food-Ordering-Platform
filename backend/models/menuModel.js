ac38680f5d17bb583d46918f72eb0c1422f2f523
Gwen <ledilegwen@gmail.com>
2026-04-11 15:55:27 +0200

    checking menumodel

--- backend/models/menuModel.js
+++ backend/models/menuModel.js
@@ -1,34 +1,96 @@
-let menu = []; // this needs to be changed to firebase datase/configs
+let menu = [
+  {
+    id: "1",
+    name: "Sample Burger",
+    price: 50,
+    description: "Default item",
+    available: true
+  }
+];
 
+// --------------------
+// ADD ITEM
+// --------------------
 function addItem(item) {
   if (!item.name || !item.price) {
     throw new Error("Invalid item");
   }
-  const newItem = { id: Date.now(), ...item, available: true };
+
+  const newItem = {
+    id: Date.now().toString(),
+    name: item.name,
+    description: item.description || "",
+    price: item.price,
+    photo: item.photo || "",
+    available: true
+  };
+
   menu.push(newItem);
   return newItem;
 }
 
+// --------------------
+// GET ITEMS
+// --------------------
 function getItems() {
   return menu;
 }
 
+// --------------------
+// MARK SOLD OUT
+// --------------------
 function markSoldOut(id) {
-  const item = menu.find(i => i.id == id);
-  if (!item) throw new Error("Item not found");
-  item.available = false;
-  return item;
-}
-
-function updateItem(id, data) {
-  const item = menu.find(i => i.id == id);
+  const item = menu.find(i => i.id === id);
 
   if (!item) {
     throw new Error("Item not found");
   }
 
-  Object.assign(item, data);
+  item.available = false;
   return item;
 }
 
-module.exports = { addItem, getItems, markSoldOut, updateItem };
+// --------------------
+// UPDATE ITEM
+// --------------------
+function updateItem(id, data) {
+  const item = menu.find(i => i.id === id);
+
+  if (!item) {
+    throw new Error("Item not found");
+  }
+
+  // only update allowed fields (prevents breaking structure)
+  const allowedFields = ["name", "description", "price", "photo", "available"];
+
+  allowedFields.forEach(field => {
+    if (data[field] !== undefined) {
+      item[field] = data[field];
+    }
+  });
+
+  return item;
+}
+
+// --------------------
+// RESET (FOR TESTING ONLY)
+// --------------------
+function __resetMenu() {
+  menu = [
+    {
+      id: "1",
+      name: "Sample Burger",
+      price: 50,
+      description: "Default item",
+      available: true
+    }
+  ];
+}
+
+module.exports = {
+  addItem,
+  getItems,
+  markSoldOut,
+  updateItem,
+  __resetMenu
+};
