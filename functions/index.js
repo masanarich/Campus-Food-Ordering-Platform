2ac0ecd02082886b60db8e4fff1df15bdd8694c9
Gwen <ledilegwen@gmail.com>
2026-04-12 03:09:55 +0200

    stuck at runtime_again_

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require("firebase-functions");
+/*const functions = require("firebase-functions");
 const express = require("express");
 
 const app = express();
@@ -9,4 +9,25 @@ app.get("/", (req, res) => {
 });
 
 // ONLY ONE ROUTE FIRST (NO IMPORTS)
+exports.api = functions.https.onRequest(app);*/
+console.log("🔥 FIREBASE START");
+
+const functions = require("firebase-functions");
+
+console.log("STEP 1 OK");
+
+const express = require("express");
+console.log("STEP 2 OK");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("WORKING");
+});
+
+console.log("STEP 3 OK");
+
+// 🔥 VERY IMPORTANT: export FIRST (no routes, no imports)
 exports.api = functions.https.onRequest(app);
+
+console.log("STEP 4 EXPORT DONE");
