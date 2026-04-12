1424ba2aa077bbb6605f629616117393aacd6009
masanarich <maduwarich@gmail.com>
2026-04-12 19:43:28 +0200

    vendor appp coverage

--- tests/authentication-tests/vendor-application.test.js
+++ tests/authentication-tests/vendor-application.test.js
@@ -2,53 +2,7 @@
  * @jest-environment jsdom
  */
 
-const fs = require("fs");
-const path = require("path");
-
-function loadModule(moduleRelativePath, injectedGlobals = {}) {
-    const filePath = path.resolve(__dirname, moduleRelativePath);
-
-    let source = fs.readFileSync(filePath, "utf8");
-
-    source = source.replace(
-        /export\s*\{[\s\S]*?\};?\s*/m,
-        ""
-    );
-
-    const moduleShim = { exports: {} };
-
-    const argNames = [
-        "module",
-        "exports",
-        "require",
-        "__filename",
-        "__dirname",
-        ...Object.keys(injectedGlobals)
-    ];
-
-    const argValues = [
-        moduleShim,
-        moduleShim.exports,
-        require,
-        filePath,
-        path.dirname(filePath),
-        ...Object.values(injectedGlobals)
-    ];
-
-    const factory = new Function(
-        ...argNames,
-        `${source}
-return module.exports;`
-    );
-
-    return factory(...argValues);
-}
-
-const authUtils = loadModule("../../public/authentication/auth-utils.js", {
-    window,
-    document
-});
-
+const authUtils = require("../../public/authentication/auth-utils.js");
 const {
     normalizeText,
     getFormField,
@@ -64,10 +18,7 @@ const {
     submitVendorApplication,
     attachVendorApplicationHandler,
     initializeVendorApplicationPage
-} = loadModule("../../public/authentication/vendor-application.js", {
-    document,
-    window
-});
+} = require("../../public/authentication/vendor-application.js");
 
 function getVendorApplicationFields(form) {
     return {
