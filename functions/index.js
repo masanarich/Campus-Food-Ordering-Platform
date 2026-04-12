e686c48326bfe33845cc332bc72c972a7ead5cfe
Gwen <ledilegwen@gmail.com>
2026-04-12 09:00:47 +0200

    second_final commi_t

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,18 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+// TEST ROUTE (must work first)
 app.get("/", (req, res) => {
   res.send("API WORKING");
 });
 
-// 🔥 IMPORTANT FIX: named function export (NOT generic api)
-exports.backend = functions.https.onRequest(app);
+// MENU ROUTES
+app.use("/menu", require("./backend/routes/menu"));
+
+// VENDOR ROUTES
+app.use("/vendors", require("./backend/routes/vendor"));
+
+// EXPORT (IMPORTANT)
+exports.api = functions.https.onRequest(app);
